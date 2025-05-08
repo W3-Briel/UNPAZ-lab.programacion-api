d0a85eefe8bd8ec24125f871932552d3756d6466
angel <saucedog926@gmail.com>
2025-05-08 20:49:02 -0300

    agregando algo de jsdocs

--- src/api/v1/controllers/operaciones.controller.js
+++ src/api/v1/controllers/operaciones.controller.js
@@ -1,3 +1,23 @@
+/**
+ * @description formato esperado por el controller, para realizar operaciones correctamente
+ * @typedef {Object} Operacion
+ * @property {number} op1 operador uno
+ * @property {number} op2 operador dos
+ * @property {string} fn nombre de una operacion
+ */
+
+/**
+ * @description formato de operacion esperado
+ * @typedef {Object} Result
+ * @property {number} op1 operador uno
+ * @property {number} op2 operador dos
+ * @property {string} fn nombre de una operacion
+ * @property {number} result resultado de la operacion
+ */
+
+/**
+ * objeto con metodos para las operaciones del endpoint
+*/
 let operaciones = {
     "suma": (a,b)=> a+b,
     "resta": (a,b)=> a-b,
@@ -6,8 +26,17 @@ let operaciones = {
     "resto": (a,b)=> a%b
 }
 
+/**
+ * 
+ * @param {Array<Operacion>} body operaciones en formato json
+ * @returns {Array<Result> | undefined}
+*/
 let operacionesController = (body)=>{
+
     let result = body.map(data => {
+
+        if (! (data.fn in operaciones) ) throw new Error("La operacion no es soportada")
+
         let op1 = data.op1
         let op2 = data.op2
         return {...data, "result": operaciones[data.fn](op1,op2)}
--- src/api/v1/routers/operaciones.router.js
+++ src/api/v1/routers/operaciones.router.js
@@ -5,12 +5,16 @@ let operacionesController = require("../controllers/operaciones.controller")
 
 router.post("/", (req,res)=>{
     let { body } = req
-    let operacionesRes = operacionesController(body)
 
-    if (!operacionesController){
-        return res.status(204).json({"messaje": 'no hay nada amigo'})
-    } 
-    return res.status(201).json(operacionesRes)
+    try {
+        let operacionesRes = operacionesController(body)
+            if (!operacionesController){
+                return res.status(204).json({"message": 'no hay nada amigo'})
+            } 
+            return res.status(201).json(operacionesRes)
+    } catch (e) {
+        return res.status(401).json({"error": e.message})
+    }
     
 })
 
